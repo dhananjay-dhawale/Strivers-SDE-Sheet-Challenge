0e78bb5b01ae6d8b7fd3a30d838b369aa8146b17
Dhananjay Dhawale <73440288+dhananjay-dhawale@users.noreply.github.com>
2023-06-05 13:03:10 +0530

    Update Pascals_triangle.cpp
    Just a simple DP.

--- Arrays - I/Pascals_triangle.cpp
+++ Arrays - I/Pascals_triangle.cpp
@@ -1,1 +1,15 @@
+#include <bits/stdc++.h>
+
+vector<vector<long long int>> printPascal(int n) 
+{
+    vector<vector<long long>> M(n);
+    for (int i = 0; i < n; i++) {
+        M[i].resize(i + 1);
+        M[i][0] = M[i][i] = 1;
+        for (int j = 1; j < i; j++) {
+            M[i][j] = M[i - 1][j - 1] + M[i - 1][j];
+        }
+    }    
+    return M;
+}
 
