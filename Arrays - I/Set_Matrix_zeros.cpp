c4e95cd3e91c62e183392096f2180ba87cfea536
Dhananjay Dhawale <73440288+dhananjay-dhawale@users.noreply.github.com>
2023-06-05 10:45:25 +0530

    Update Set_Matrix_zeros.cpp
    Whenever you come across any element which is 0 set that row number and column number to 1. Then again iterate the matrix if row number or column number is set then set the value equal to 0

--- Arrays - I/Set_Matrix_zeros.cpp
+++ Arrays - I/Set_Matrix_zeros.cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
 
+void setZeros(vector<vector<int>> &M)
+{
+	int n = M.size(), m = M[0].size();
+	vector <int> r(n), c(m);
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < m; j++) {
+			if(!M[i][j]) {
+				r[i] = 1;
+				c[j] = 1;
+			}
+		} 
+	}
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < m; j++) {
+            if (r[i] || c[j]) {
+				M[i][j] = 0;
+            }
+        }
+    }
+}
