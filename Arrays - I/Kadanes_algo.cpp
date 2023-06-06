8107faeae03ae877d6a979c4da14165da7858a2e
Dhananjay Dhawale <73440288+dhananjay-dhawale@users.noreply.github.com>
2023-06-06 13:01:44 +0530

    Update Kadanes_algo.cpp

--- Arrays - I/Kadanes_algo.cpp
+++ Arrays - I/Kadanes_algo.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    int maxSubArray(vector<int>& arr) {
+        int n = arr.size();
+        long long local_sum = arr[0], global_sum = arr[0];
+        for(int i = 1; i < n; i++) {
+            local_sum  = max(1LL * arr[i], local_sum + arr[i]);
+            global_sum = max(global_sum, local_sum);
+        }
+        return global_sum;
+    }
+};
