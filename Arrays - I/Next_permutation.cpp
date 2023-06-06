eb78f68fb4a35660aa44ebbf7284a3a0ff72e387
Dhananjay Dhawale <73440288+dhananjay-dhawale@users.noreply.github.com>
2023-06-06 12:51:41 +0530

    Update Next_permutation.cpp
    Iterate from the back, you have to find first position where adjacent element are increasing, then swap that element with the minimum element encountered till now, and reverse the decreasing subarray encountered till now

--- Arrays - I/Next_permutation.cpp
+++ Arrays - I/Next_permutation.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+public:
+    void nextPermutation(vector<int>& P) {
+        int j = 0, n = P.size();
+        for(int i = n - 1; i > 0; i--) {
+            if(P[i] > P[i - 1]) {
+                j = i;
+                break;
+            }
+        }
+        if(j) {
+            int i = j;
+            while(i < n && P[i] > P[j - 1]) {
+                i += 1;
+            }
+            i -= 1;
+            swap(P[i], P[j - 1]);
+        }
+        int l = j, r = n - 1;
+        while(l < r) {
+            swap(P[l++], P[r--]);
+        }
+    }
+};
