6129dddb39716853d6991ad265e6b41140b590c7
Dhananjay Dhawale <73440288+dhananjay-dhawale@users.noreply.github.com>
2023-06-07 18:44:42 +0530

    Update Rotate_Matrix.cpp

--- Arrays - II/Rotate_Matrix.cpp
+++ Arrays - II/Rotate_Matrix.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    void rotate(vector<vector<int>>& M) {
+        int n = M.size();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                swap(M[i][j], M[j][i]);
+            }
+        }
+        for(auto &i : M) {
+            reverse(i.begin(), i.end());
+        }
+    }
+};
