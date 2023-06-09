42f449eec5f708bb639a35ac1967179a96d1a2f0
Dhananjay Dhawale <73440288+dhananjay-dhawale@users.noreply.github.com>
2023-06-09 13:55:21 +0530

    Update Merge_overlapping_subintervals.cpp

--- Arrays - II/Merge_overlapping_subintervals.cpp
+++ Arrays - II/Merge_overlapping_subintervals.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& I) {
+        int n = I.size();
+        sort(I.begin(), I.end());
+        vector <vector <int>> v;
+        v.push_back(I[0]);
+        for(int i = 1; i < n; i++) {
+            if(I[i][0] <= v.back()[1]) {
+                v.back()[1] = max(v.back()[1], I[i][1]);
+            }
+            else {
+                v.push_back(I[i]);
+            }
+        }
+        return v;
+    }
+};
